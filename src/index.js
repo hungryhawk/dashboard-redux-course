fdd1a9606bfa1123799fb8f2098cb48cbd1bd743
Pavel Emelianov <emeliarulit@gmail.com>
2023-01-19 12:53:30 +0300

    add data and positions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { store } from './store';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
